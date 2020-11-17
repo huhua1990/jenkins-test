dbbcd1c59181923157ef682a0d77e3be756e0ed0
wb-hh462640 <wb-hh462640@alibaba-inc.com>
2020-11-17 10:33:22 +0800

    add first

---
+++ jenkins/src/main/java/com/jenkins/test/JenkinsApplication.java
@@ -0,0 +1,28 @@
+package com.jenkins.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description
+ * @Auther: hh
+ * @Date: 2020/11/17 10:22
+ * @Version:1.0
+ */
+@RestController
+@SpringBootApplication
+public class JenkinsApplication {
+
+    @RequestMapping("/jenkins")
+    String index() {
+        return "Hello Spring Boot";
+
+    }
+
+    //作为项目启动入口
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsApplication.class, args);
+    }
+}
